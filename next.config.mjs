cc44d49330802617847771202a6f15a7c47dccc7
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2024-08-10 11:50:33 -0400

    Fixed next js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 // const fs = require('fs');
-import * as fs from "fs"
-import * as dotenv from "dotenv";
+// import * as fs from "fs"
+// import * as dotenv from "dotenv";
 
-// const dotenv = require('dotenv');
-const env = dotenv.parse(fs.readFileSync('.env'));
 
 const nextConfig = {
     reactStrictMode: true,
-    env: env
   }
 export default nextConfig;
